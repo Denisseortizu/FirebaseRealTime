e9b762116f80e2bb3fea3f032730da7008285706
Denisseortizu <denisseortizu@gmail.com>
2021-12-06 18:21:54 -0600

    segundo commit

--- app/src/main/java/com/example/firebaserealtime/MainActivity.kt
+++ app/src/main/java/com/example/firebaserealtime/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.firebaserealtime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -18,8 +19,25 @@ class MainActivity : AppCompatActivity() {
         database.setValue("Hola Mundo!")
         database.child("hijo").child("nieto").setValue("Nieto")
 
-
+        writeNewUser("001", "nombre2", "email2@correo.com")
         //database.child("usuarios").setValue("primer usuario")
         //database.child("usuarios").setValue("segundo usuario")
     }
+
+    fun getUser(userID: String){
+        database.child("users").child(userID).get().addOnSuccessListener { record ->
+            Log.d("ValoresDeFireBase", "Got value ${record.value}")
+        }.addOnFailureListener{ record ->
+            Log.d("VlauesDeFireBase", "Error: $record")
+        }
+    }
+
+    fun writeNewUser(userID: String, name: String, email: String){
+        val user = User(name, email)
+        database.child("users").child(userID).setValue(user)
+    }
+}
+class User(name: String, email: String){
+    val nombre = name
+    val correo = email
 }
